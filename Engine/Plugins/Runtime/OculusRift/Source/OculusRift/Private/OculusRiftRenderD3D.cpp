d5efaa8337435c6be2e6b8879f51386827dc3341
Artem Bolgar <artem@oculusvr.com>
2015-07-09 01:40:18 -0400

    fixing 'show rendering' mode to show timewarp

--- Engine/Plugins/Runtime/OculusRift/Source/OculusRift/Private/OculusRiftRenderD3D.cpp
+++ Engine/Plugins/Runtime/OculusRift/Source/OculusRift/Private/OculusRiftRenderD3D.cpp
@@ -507,7 +507,10 @@ void FOculusRiftHMD::D3D11Bridge::FinishRendering()
 
 			ovrHmd_SubmitFrame(RenderContext->Hmd, RenderContext->RenderFrame->FrameNumber, &viewScaleDesc, LayerList, 1);
 
-			ColorTextureSet->SwitchToNextElement();
+			if (RenderContext->ShowFlags.Rendering)
+			{
+				ColorTextureSet->SwitchToNextElement();
+			}
 		}
 	}
 	else
