38ff713aff182797e56afde345a77d7acc33e555
Artem Bolgar <artem@oculusvr.com>
2015-09-30 01:36:33 -0700

    Fixing potential perf issue in 'pause' mode, as well as losing low persistence mode in pause mode on DK2.

--- Engine/Source/Runtime/Engine/Private/PlayerController.cpp
+++ Engine/Source/Runtime/Engine/Private/PlayerController.cpp
@@ -4598,7 +4598,7 @@ void APlayerController::OnServerStartedVisualLogger_Implementation(bool bIsLoggi
 
 bool APlayerController::ShouldPerformFullTickWhenPaused() const
 {
-	bool bIsInVR = (GEngine->HMDDevice.IsValid() && GEngine->HMDDevice->IsStereoEnabled() && GEngine->HMDDevice->IsHMDConnected());
+	bool bIsInVR = (GEngine->HMDDevice.IsValid() && GEngine->HMDDevice->IsStereoEnabled() /*&& GEngine->HMDDevice->IsHMDConnected()*/);
 	return bIsInVR || bShouldPerformFullTickWhenPaused;
 }
 
