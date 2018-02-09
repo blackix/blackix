78b8a1ed9fc0fc56e3f38eab503277efb9129405
Loren McQuade <loren.mcquade@oculus.com>
2018-02-08 16:24:37 -0800

    Partner-Oculus-4.19 @3878211
    OVRPlugin 1.22.0
    Platform SDK 1.20.0
    Audio SDK 1.18.0

--- Engine/Plugins/Runtime/Oculus/OculusVR/Source/OculusHMD/Public/OculusFunctionLibrary.h
+++ Engine/Plugins/Runtime/Oculus/OculusVR/Source/OculusHMD/Public/OculusFunctionLibrary.h
@@ -87,6 +87,7 @@ enum class EGearVRControllerHandedness_DEPRECATED : uint8
 	Unknown_DEPRECATED
 };
 
+/** DEPRECATED in 4.17 : All functions using this enum have been deprecated and it will be removed with them. If you are still needing this, then please create your own replacement.*/
 UENUM(BlueprintType)
 enum class ETiledMultiResLevel : uint8
 {
